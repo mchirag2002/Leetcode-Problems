fd6b3a5a673bfcf6ff9d47f0e28c7d3bf3c52757
Chirag Mathur <mchirag2002@gmail.com>
2022-06-24 01:17:19 +0530

    new Problems

---
+++ Roman_to_integer.java
@@ -0,0 +1,102 @@
+public class Roman_to_integer {
+    public static void main(String[] args) {
+        String s = "XIV";
+        int count = 0;
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c=='I')
+            {
+                if(i<s.length()-1)
+                {
+                    char d = s.charAt(i+1);
+                    if(d=='V')
+                    {
+                        count+=4;
+                        i++;
+                    }
+                    else if(d=='X')
+                    {
+                        count+=9;
+                        i++;
+                    }
+                    else
+                    {
+                        count+=1;
+                    }
+                }
+                else
+                {
+                    count+=1;
+                }
+            }
+            else if(c=='V')
+            {
+                count+=5;
+            }
+            else if(c=='X')
+            {
+                if(i<s.length()-1)
+                {
+                    char d = s.charAt(i+1);
+                    if(d=='L')
+                    {
+                        count+=40;
+                        i++;
+                    }
+                    else if(d=='C')
+                    {
+                        count+=90;
+                        i++;
+                    }
+                    else
+                    {
+                        count+=10;
+                    }
+                }
+                else
+                {
+                    count+=10;
+                }
+            }
+            else if(c=='L')
+            {
+                count+=50;
+            }
+            else if(c=='C')
+            {
+                if(i<s.length()-1)
+                {
+                    char d = s.charAt(i+1);
+                    if(d=='D')
+                    {
+                        count+=400;
+                        i++;
+                    }
+                    else if(d=='M')
+                    {
+                        count+=900;
+                        i++;
+                    }
+                    else
+                    {
+                        count+=100;
+                    }
+                }
+                else
+                {
+                    count+=100;
+                }
+            }
+            else if(c=='D')
+            {
+                count+=500;
+            }
+            else
+            {
+                count+=1000;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ Valid_parenthesis.java
@@ -0,0 +1,98 @@
+public class Valid_parenthesis {
+    public static void main(String[] args) {
+        String s = "(([]){})";
+        String str = "";
+        String ptr = "";
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+                //str.concat("(");
+                str = str+"(";
+            }
+            else if(s.charAt(i)=='{')
+            {
+                //str.concat("{");
+                str+="{";
+            }
+            else if(s.charAt(i)=='[')
+            {
+                //str.concat("[");
+                str+="[";
+            }
+            else if(s.charAt(i)==')')
+            {
+                if(str.endsWith("("))
+                {
+                    if(str.length()>1)
+                    {ptr = "";
+                        for(int j = 0; j<str.length()-1; j++)
+                        {
+                            //ptr.concat(String.valueOf(str.charAt(j)));
+                            ptr+=str.charAt(j);
+                        }
+                        str = ptr;
+                    }
+                    else
+                    {
+                        str = "";
+                    }
+                }
+                else
+                {
+                    System.out.println("false");
+                }
+            }
+            else if(s.charAt(i)=='}')
+            {
+                if(str.endsWith("{"))
+                {
+                    if(str.length()>1)
+                    {ptr = "";
+                        for(int j = 0; j<str.length()-1; j++)
+                        {
+                            //ptr.concat(String.valueOf(str.charAt(j)));
+                            ptr+=str.charAt(j);
+                        }
+                        str = ptr;}
+                    else
+                    {
+                        str = "";
+                    }
+                }
+                else
+                {
+                    System.out.println("false");
+                }
+            }
+            else if(s.charAt(i)==']')
+            {
+                if(str.endsWith("["))
+                {
+                    if(str.length()>1)
+                    {ptr = "";
+                        for(int j = 0; j<str.length()-1; j++)
+                        {
+                            //ptr.concat(String.valueOf(str.charAt(j)));
+                            ptr+=str.charAt(j);
+                        }
+                        str = ptr;}
+                    else
+                    {
+                        str = "";
+                    }
+                }
+                else
+                {
+                    System.out.println("false");
+                }
+            }
+        }
+        if(str.length()==0)
+        {
+            System.out.println("true");
+        }
+        else
+            System.out.println("false");
+    }
+}
